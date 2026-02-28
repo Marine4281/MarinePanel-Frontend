cbc673a9e4660442aafad680a56fdaa86364d5ca
Marine4281 <marinepro360@gmail.com>
2026-02-28 14:24:49 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,6 @@ import AdminService from "./pages/AdminService";
 import AdminSettings from "./pages/AdminSettings";
 import AdminPaymentMethods from "./pages/AdminPaymentMethods";
 import AdminOrders from "./pages/AdminOrders";
-import AdminUserOrders from "./pages/AdminUserOrders";
 
 import ProtectedRoute from "./components/ProtectedRoute";
 
@@ -120,15 +119,7 @@ export default function App() {
             </ProtectedRoute>
           }
         />
-       <Route
-         path="/admin/user-orders"
-         element={
-          <ProtectedRoute adminOnly>
-            <AdminUserOrders />
-          </ProtectedRoute>
-        }
-       />
       </Routes>
     </AuthProvider>
   );
-        }
+                }
