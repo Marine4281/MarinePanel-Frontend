dd698b60ca42d91e4bcd304fb30e4fa6c8b3d42d
Marine4281 <marinepro360@gmail.com>
2026-03-04 18:50:46 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+
 import "./index.css";
 import App from "./App.jsx";
+
+// 🔥 NEW
+import { ServicesProvider } from "./context/ServicesContext";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <ServicesProvider>
+        <App />
+      </ServicesProvider>
     </BrowserRouter>
   </StrictMode>
 );
