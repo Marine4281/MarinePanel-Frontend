93acd3386171d25710f5c3ba4661a3b0eaca9c77
Marine4281 <marinepro360@gmail.com>
2026-03-15 15:16:20 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { ResellerProvider } from "./context/ResellerContext";
 
-
-import "./index.css";
 import App from "./App.jsx";
+import "./index.css";
 
-// 🔥 NEW
+// Context Providers
 import { ServicesProvider } from "./context/ServicesContext";
+import { ResellerProvider } from "./context/ResellerContext";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <ServicesProvider>
-        <App />
-      </ServicesProvider>
+      <ResellerProvider>
+        <ServicesProvider>
+          <App />
+        </ServicesProvider>
+      </ResellerProvider>
     </BrowserRouter>
   </StrictMode>
 );
