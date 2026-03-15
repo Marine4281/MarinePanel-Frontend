b2c593e5128e804e1e3736c73144a791a31fa0f2
Marine4281 <marinepro360@gmail.com>
2026-03-15 15:06:46 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { ResellerProvider } from "./context/ResellerContext";
+
 
 import "./index.css";
 import App from "./App.jsx";
