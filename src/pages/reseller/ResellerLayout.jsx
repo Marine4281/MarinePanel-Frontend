d7c4509b86f1c6aff9e418aa4b0358adfe9f4b46
Marine4281 <marinepro360@gmail.com>
2026-03-21 20:38:18 +0300

    Create ResellerLayout.jsx

---
+++ src/pages/reseller/ResellerLayout.jsx
@@ -0,0 +1,17 @@
+import Sidebar from "../../components/reseller/Sidebar";
+
+const ResellerLayout = ({ children }) => {
+  return (
+    <div className="flex min-h-screen bg-gray-100">
+      {/* Sidebar */}
+      <Sidebar />
+
+      {/* Main Content */}
+      <div className="flex-1 p-4 overflow-y-auto">
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default ResellerLayout;
