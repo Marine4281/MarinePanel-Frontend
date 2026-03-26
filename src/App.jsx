ededbe5c943bfc4244ce519b780add88ee03001c
Marine4281 <marinepro360@gmail.com>
2026-03-26 19:39:56 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -37,8 +37,8 @@ import ResellerOrders from "./pages/reseller/ResellerOrders";
 import ResellerBranding from "./pages/reseller/ResellerBranding";
 import ResellerServices from "./pages/reseller/ResellerServices";
 import EndUserDashboard from "./pages/reseller/EndUserDashboard";
-import ResellersList from "./pages/reseller/ResellersList";
-import ResellerDetails from "./pages/reseller/ResellerDetails";
+import ResellersAdminList from "./pages/reseller/ResellersAdminList";
+import ResellerAdminDetails from "./pages/reseller/ResellerAdminDetails";
 
 
 
