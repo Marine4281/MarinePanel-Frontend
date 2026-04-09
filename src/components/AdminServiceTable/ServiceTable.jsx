468e98c16faea0a68d23f2632e4e11a6c030e52f
Marine4281 <marinepro360@gmail.com>
2026-04-09 23:44:36 +0300

    Create ServiceTable.jsx

---
+++ src/components/AdminServiceTable/ServiceTable.jsx
@@ -0,0 +1,9 @@
+if (!groupedServices.length) {
+  return (
+    <div className="space-y-3">
+      {[...Array(5)].map((_, i) => (
+        <div key={i} className="h-10 bg-gray-200 animate-pulse rounded" />
+      ))}
+    </div>
+  );
+}
