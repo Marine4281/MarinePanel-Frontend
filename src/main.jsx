7a12e36f486d649b194554683f0f400d2b25698a
Marine4281 <marinepro360@gmail.com>
2026-05-03 00:06:38 +0300

    Add ChildPanelProvider to the rendering tree

--- src/main.jsx
+++ src/main.jsx
@@ -8,15 +8,18 @@ import "./index.css";
 // Context Providers
 import { ServicesProvider } from "./context/ServicesContext";
 import { ResellerProvider } from "./context/ResellerContext";
+import { ChildPanelProvider } from "./context/ChildPanelContext";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
+      <ChildPanelProvider>
       <ResellerProvider>
         <ServicesProvider>
           <App />
         </ServicesProvider>
       </ResellerProvider>
+      </ChildPanelProvider>
     </BrowserRouter>
   </StrictMode>
 );
