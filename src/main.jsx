a140c9dd51aaa14c0b1f62b35e39f127619f7845
Marine4281 <marinepro360@gmail.com>
2026-05-03 09:29:57 +0300

    Simplify main.jsx by removing context providers
    Removed context providers from the main render method.

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,15 @@
+// src/main.jsx
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-
 import App from "./App.jsx";
 import "./index.css";
-
-// Context Providers
-import { ServicesProvider } from "./context/ServicesContext";
-import { ResellerProvider } from "./context/ResellerContext";
-import { ChildPanelProvider } from "./context/ChildPanelContext";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <ChildPanelProvider>
-      <ResellerProvider>
-        <ServicesProvider>
-          <App />
-        </ServicesProvider>
-      </ResellerProvider>
-      </ChildPanelProvider>
+      <App />
     </BrowserRouter>
   </StrictMode>
 );
