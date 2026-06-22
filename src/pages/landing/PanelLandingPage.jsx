faf5635ec475be8e6a82719ccafd303ad4f6f418
Marine4281 <marinepro360@gmail.com>
2026-06-22 11:27:09 +0300

    Add new landing templates to PanelLandingPage

--- src/pages/landing/PanelLandingPage.jsx
+++ src/pages/landing/PanelLandingPage.jsx
@@ -1,18 +1,19 @@
-// src/pages/landing/PanelLandingPage.jsx
-//
-// Renders the correct landing template for child panel and reseller domains.
-// Called by LandingPage.jsx when domainType === "childPanel" || "reseller".
-
 import DefaultLanding  from "./templates/DefaultLanding";
 import DarkProLanding  from "./templates/DarkProLanding";
 import MinimalLanding  from "./templates/MinimalLanding";
 import VibrantLanding  from "./templates/VibrantLanding";
+import SunriseLanding  from "./templates/SunriseLanding";
+import BoldLanding     from "./templates/BoldLanding";
+import NeonLanding     from "./templates/NeonLanding";
 
 const TEMPLATES = {
   "default":  DefaultLanding,
   "dark-pro": DarkProLanding,
   "minimal":  MinimalLanding,
   "vibrant":  VibrantLanding,
+  "sunrise":  SunriseLanding,
+  "bold":     BoldLanding,
+  "neon":     NeonLanding,
 };
 
 export default function PanelLandingPage({ brandName, themeColor, logo, landingTemplate }) {
