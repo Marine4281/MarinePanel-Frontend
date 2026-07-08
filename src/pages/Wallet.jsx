748abc81ecc1b7e51917b48306d1901274edbf52
Marine4281 <marinepro360@gmail.com>
2026-07-08 11:40:08 +0300

    Update withdrawal action to navigate to withdraw page

--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -54,7 +54,7 @@ const Wallet = () => {
           setBalance={setBalance}
           setTransactions={setTransactions}
           onAddFunds={() => navigate("/add-funds")}
-          onWithdraw={() => toast("Withdrawal coming soon", { icon: "⏳" })}
+          onWithdraw={() => navigate("/withdraw)}
         />
 
         <TransactionHistory transactions={transactions} />
