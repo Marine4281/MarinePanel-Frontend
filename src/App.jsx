a5b46fa50b6f47222a7fd54ec8a7f652354cd2d4
Marine4281 <marinepro360@gmail.com>
2026-08-14 19:27:50 +0300

    Fix formatting for Route component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -265,7 +265,7 @@ function AppRoutes() {
 
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/reset-password/:token" element={<ResetPassword />} />
-        <Route path="/api-access" element={<TemplateRouter page="apiDocs" defaultPage={<ApiDocsPage />} />}
+        <Route path="/api-access" element={<TemplateRouter page="apiDocs" defaultPage={<ApiDocsPage />} />}/>
         <Route path="/terms" element={<ProtectedRoute><TermsAndConditions /></ProtectedRoute>} />
         <Route path="/terms-public" element={<TermsPublic />} />
 
